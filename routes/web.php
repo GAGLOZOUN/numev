5eb2e8ebf8bca6121ab074ba14e1ebebda812084
GAGLOZOUN <gaglozoungagnon@gmail.com>
2022-12-13 08:16:02 +0100

    intagration template confifuration des routes et la modification du template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/service', function () {
+    return view('service');
+});
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/service2', function () {
+    return view('service2');
+});
+Route::get('/service3', function () {
+    return view('service3');
+});
+Route::get('/service4', function () {
+    return view('service4');
+});
+Route::get('/service5', function () {
+    return view('service5');
+});
+Route::get('/service6', function () {
+    return view('service6');
+});
